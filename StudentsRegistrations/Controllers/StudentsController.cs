f00eaf41238b19ab850b7b118c04b526b050a4cc
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2022-05-25 17:33:28 +0300

    add controller for server

---
+++ StudentsRegistrations/Controllers/StudentsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudentsRegistrations.Models;
+using StudentsRegistrations.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsRegistrations.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : Controller
+    {
+        private readonly IStudentServices _studentsServices;
+        public StudentsController(IStudentServices studentServices)
+        {
+            _studentsServices = studentServices;
+        }
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            //return View("StudentsView", _studentsServices.GetStudents());
+            return Ok(_studentsServices.GetStudents());
+        }
+        //[HttpGet("{id}")]
+        //public IActionResult GetStudent(string studentId)
+        //{
+        //    return Ok(_studentsServices.GetStudent(studentId));
+        //}
+        [Route("/unSubmitted")]
+        [HttpGet]
+        public IActionResult GetUnsubmittedStudents()
+        {
+            return Ok(_studentsServices.GetUnsubmittedStudents());
+        }
+        //[HttpGet("{country}")]
+        //public IActionResult GetStudentsFromCountryCount(string country)
+        //{
+        //    return Ok(_studentsServices.countStudentsFromCountry(country));
+        //}
+        [HttpPost]
+        public IActionResult AddStudent([FromBody] Student newStudent)
+        {
+            _studentsServices.AddStudent(newStudent);
+            //return CreatedAtRoute("Students", new { id = newStudent.id }, newStudent);
+            return CreatedAtAction(nameof(AddStudent), new { newStudent.id }, newStudent);
+        }
+        //[HttpDelete("{id}")]
+        //public IActionResult DeleteStudent(string studentId)
+        //{
+        //    _studentsServices.DeleteStudent(studentId);
+        //    return NoContent();
+        //}
+        //[HttpPut]
+        //public IActionResult UpdateStudent(Student student)
+        //{
+        //    return Ok(_studentsServices.UpdateStudent(student));
+        //}
+    }
+}
